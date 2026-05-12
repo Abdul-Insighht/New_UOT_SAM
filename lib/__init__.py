3f880e488babc7d07bbc8792d659c3540a5de4e3
Hafiz Abdul Rehman <Abdul-Insighht@users.noreply.github.com>
2026-05-12 17:34:48 +0500

    Fix ImportError: Remove deleted modules from __init__.py

--- lib/__init__.py
+++ lib/__init__.py
@@ -10,8 +10,4 @@ from .enhanced_model import Enhanced_RRSIS_UOT
 from .dynamic_lora import DynamicLoRALinear, inject_dynamic_lora_adapters
 from .contrastive_loss import ContrastiveLoss
 from .multiscale_ot_alignment import MultiScaleOTAligner
-from .ohem_loss import EnhancedOHEMLoss, OHEMLoss, FocalDiceLoss, BoundaryAwareLoss, LovaszHingeLoss
-
-# Performance enhancement modules (NEW - Pr@0.8/0.9 improvements)
-from .orientation_conv import OrientationAwareConv
-from .mask_refinement import MaskRefinementHead
+from .ohem_loss import EnhancedOHEMLoss, OHEMLoss, FocalDiceLoss
